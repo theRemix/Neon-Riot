799393806e0667b6118f736fc8240ec7a55e2622
Jon Borgonia <jon@gomagames.com>
2017-06-28 18:04:22 -1000

    :lipstick: fix scaling on primary/TrianglePrism

--- components/primary/TrianglePrism.js
+++ components/primary/TrianglePrism.js
@@ -24,9 +24,11 @@ export default ({ windowSize, seed }) => {
 
   // @TODO to come from constants
   const size = {
-    width : 400,
-    height : 400,
+    width : 800,
+    height : 800,
   };
+
+  const offsetForGlow = 10;
 
   const {
     GlowFilter : GlowFilterBase,
@@ -37,22 +39,22 @@ export default ({ windowSize, seed }) => {
     GlowFilterDef : GlowFilterDefInner
   } = GlowFilterProvider();
 
+  const sizeOffset = size.width;
   return (
     <svg
-      { ...size }
-      x={( windowSize.width / 2 ) - ( size.width / 2 )}
-      y={( windowSize.height / 2 ) - ( size.height / 2 )}
-      viewBox="0 0 240 240"
-      preserveAspectRatio="none">
+      width={size.width+offsetForGlow+sizeOffset}
+      height={size.height+offsetForGlow+sizeOffset}
+      x={( windowSize.width / 2 ) - ( size.width / 4 ) + offsetForGlow/2}
+      y={( windowSize.height / 2 ) - ( size.height / 4 ) + offsetForGlow/2} >
       <defs>
         <GlowFilterDefBase color={glowColorBase} />
         <GlowFilterDefInner color={glowColorInner} />
       </defs>
 
-      <g transform="translate(20,20)" filter={ GlowFilterBase }>
+      <g transform="translate(0,0) scale(2)" filter={ GlowFilterBase }>
         <polygon { ...polyProps } points="101.58 2.3 1.99 174.8 201.18 174.8 101.58 2.3"/>
       </g>
-      <g transform="translate(33,36)" filter={ GlowFilterInner }>
+      <g transform="translate(25,30) scale(2)" filter={ GlowFilterInner }>
         <polygon points="88.91 107.1 0 154 85.07 100.45 88.91 0 92.75 100.45 177.82 154 88.91 107.1"/>
       </g>
     </svg>
